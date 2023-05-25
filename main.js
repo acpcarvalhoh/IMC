07d1b68165d4862d7af82b2fdaa052596541d794
acpcarvalhoh <carvalhohuzumak@gmail.com>
2023-05-25 11:48:28 -0300

    inplementado controle nos campos de input

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ let weight = document.querySelector('#weight')
 let height = document.querySelector('#height')
 const modal = document.querySelector('.modal-wrapper')
 const closeM = document.querySelector('.close')
+const erroMessage = document.querySelector('.alert-error')
+const btnSubmit = form.querySelector('button')
 
 
 /* Eventos */
 form.addEventListener('submit', sbtForm)
 closeM.addEventListener('click', closeModal)
+weight.addEventListener('input', alertError)
+height.addEventListener('input', alertError)
+
 
 
 /* Funções */
@@ -32,6 +37,17 @@ function closeModal(){
    height.value = ''
 }
 
+function alertError(){
+    if(isNaN(weight.value) || isNaN(height.value)){
+        erroMessage.classList.add('error-message')
+        btnSubmit.disabled = true
+    }else{
+        erroMessage.classList.remove('error-message')
+        btnSubmit.disabled = false
+    }
+}
+
 function toogleScreen(){
     modal.classList.toggle('open')
+    
 }
